68da5a91815243918df62265e3e7b96b0d68e977
vladar91@gmail.com <969721vv>
2023-02-24 17:33:04 +0200

    06 - Added logger

--- src/main/java/com/backendless/animalfarmtestproject/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/backendless/animalfarmtestproject/controllers/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.backendless.animalfarmtestproject.controllers;
 
 import com.backendless.animalfarmtestproject.exceptions.AnimalException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AnimalException.class)
     public ResponseEntity<String> handleAnimalException(AnimalException e) {
+        log.info(e.getErrorMessage());
         return ResponseEntity.ok(e.getErrorMessage());
     }
 }
