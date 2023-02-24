6aaac6a9f4e89eda8804d14ba065d7484783c798
vladar91@gmail.com <969721vv>
2023-02-24 19:47:56 +0200

    10 - added unit test

--- src/test/java/com/backendless/animalfarmtestproject/services/LionServiceImplTest.java
+++ src/test/java/com/backendless/animalfarmtestproject/services/LionServiceImplTest.java
@@ -28,23 +28,36 @@ class LionServiceImplTest {
 
     private CarnivorousService<LionModel> lionService;
 
+    private LionModel lionModel;
+
     @BeforeEach
     void setUp() {
         lionService = new LionServiceImpl(lionRepository, cowService, goatService);
+        lionModel = new LionModel();
+        lionModel.setName("testLion");
+        lionModel.setEaten(new ArrayList<>());
+        Mockito.when(lionRepository.getLionModelByName("testLion")).thenReturn(Optional.of(lionModel));
     }
 
     @Test
-    public void feedLion() {
-        LionModel lionModel = new LionModel();
-        lionModel.setName("testLion");
-        lionModel.setEaten(new ArrayList<>());
+    public void feedLionByCow() {
         CowModel cowModel = new CowModel();
         cowModel.setName("testCow");
-        Mockito.when(lionRepository.getLionModelByName("testLion")).thenReturn(Optional.of(lionModel));
         Mockito.when(cowService.getAnimalByName("testCow")).thenReturn(cowModel);
         lionService.feedLion("testLion", "testCow", "cow");
         cowModel.eaten(lionModel);
         lionModel.eat(cowModel);
+        Mockito.verify(lionRepository).save(lionModel);
+    }
+
+    @Test
+    public void feedGoatByGoat() {
+        GoatModel goatModel = new GoatModel();
+        goatModel.setName("testGoat");
+        Mockito.when(goatService.getAnimalByName("testGoat")).thenReturn(goatModel);
+        lionService.feedLion("testLion", "testGoat", "goat");
+        goatModel.eaten(lionModel);
+        lionModel.eat(goatModel);
         Mockito.verify(lionRepository).save(lionModel);
     }
 
