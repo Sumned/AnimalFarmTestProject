90164c1e2fa5f8ba65fa9007097262dc0f92871a
vladar91@gmail.com <969721vv>
2023-02-24 14:14:10 +0200

    01 - refactoring

--- src/main/java/com/backendless/animalfarmtestproject/models/CowModel.java
+++ src/main/java/com/backendless/animalfarmtestproject/models/CowModel.java
@@ -1,10 +1,6 @@
 package com.backendless.animalfarmtestproject.models;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
--- src/main/java/com/backendless/animalfarmtestproject/models/GrassModel.java
+++ src/main/java/com/backendless/animalfarmtestproject/models/GrassModel.java
@@ -2,14 +2,9 @@ package com.backendless.animalfarmtestproject.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "grass")
-@Setter
-@Getter
 public class GrassModel extends Vegetable {
-
 
 }
